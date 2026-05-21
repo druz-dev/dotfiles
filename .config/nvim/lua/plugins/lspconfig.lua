488449d85a924fbc35e5d0a3d790ef1a5f71389c
Oliver Tangen <oliver.tangen@appear.net>
2026-05-21 18:15:36 +0200

    lazy nvim plugins etc

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -1,11 +1,15 @@
 return {
   "neovim/nvim-lspconfig",
   opts = {
-    inlay_hints = { enabled = false },
-    setup = {
-      clangd = function(_, opts)
-        opts.capabilities.offsetEncoding = { "utf-16" }
-      end,
+    servers = {
+      clangd = {
+        cmd = {
+          "clangd",
+          "--query-driver=**/aarch64-appear-linux-g++",
+          "--clang-tidy",
+        },
+        autostart = true,
+      },
     },
   },
 }
--- .config/nvim/lua/plugins/modicator.lua
+++ .config/nvim/lua/plugins/modicator.lua
@@ -1,13 +1,13 @@
 return {
-  {
-    "mawkler/modicator.nvim",
-    dependencies = "folke/tokyonight.nvim",
-    init = function()
-      -- These are required for Modicator to work
-      vim.o.cursorline = true
-      vim.o.number = true
-      vim.o.termguicolors = true
-    end,
-    opts = {},
-  },
+	{
+		"mawkler/modicator.nvim",
+		dependencies = "folke/tokyonight.nvim",
+		init = function()
+			-- These are required for Modicator to work
+			vim.o.cursorline = false
+			vim.o.number = true
+			vim.o.termguicolors = true
+		end,
+		opts = {},
+	},
 }
